a075cda6a3e77ebd71908b7cbf6df724cd7e1c15
Zachary Boelter <zboelter@gmail.com>
2022-08-30 15:54:20 -0700

    split string into substring

--- problemSet1.py
+++ problemSet1.py
@@ -1,11 +1,13 @@
 print('hello world')
 
 # print('What is your name?')
-name = input('What is your name? ')
+name = input('What is your name? ') 
 
 
 # strip removes whitespace from str and title capitalizes every first letter of word in str
 name = name.strip().title()
 
+#split users data into two seperate strings
+first, middle, last = name.split(' ')
 
-print(f'Hello, {name}.')
+print(f'Hello, {middle}.')
